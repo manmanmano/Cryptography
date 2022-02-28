89b689ce3208c1e56cb8208003acfbe87d88c565
manmanmano <jimiskools@gmail.com>
2022-03-01 00:54:10 +0200

    added code to solve task 4

---
+++ task_4.py
@@ -0,0 +1,27 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet += ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def to_caesar(text, shift):
+    result = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift 
+        result += alphabet[new_position]
+    return result
+
+def transpose(text, key): 
+    key_length = len(key)
+    blocks = [text[i : i + key_length] for i in range(0, len(text), key_length)]
+    cipher = ""
+    for block in blocks:
+        for i in range(0, key_length):
+            cipher += block[key[i] - 1]
+    return cipher
+
+
+msg = "MOTIVATION"
+s1 = to_caesar(msg.lower(), 17)
+p1 = transpose(s1.lower(), [5, 1, 3, 2, 4])
+s2 = to_caesar(p1.lower(), 8)
+p2 = transpose(s2.lower(), [3, 4, 5, 1 ,2])
+print(f"The final ciphertext is: {p2}")
