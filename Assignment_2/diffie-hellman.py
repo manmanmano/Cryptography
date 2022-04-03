ccdb785d3468c4ff06c15df1c94a099ab25460f5
manmanmano <jimiskools@gmail.com>
2022-04-04 01:07:19 +0300

    diffie-hellman done

---
+++ Assignment_2/diffie-hellman.py
@@ -0,0 +1,15 @@
+def establish_key(shared_secret, a_priv, b_priv, p):
+    a_pub = shared_secret**a_priv%p
+    b_pub = shared_secret**b_priv%p
+    a_key = b_pub**a_priv%p
+    b_key = a_pub**b_priv%p
+    if a_key == b_key:
+        print("They key has been successfully established!")
+        print(f"The key is: {a_key}")
+
+    else:
+        exit("An error has occured!\n")
+
+
+shared_secret, alice_priv, bob_priv, p = 3, 95, 240, 673
+establish_key(shared_secret, alice_priv, bob_priv, p)
